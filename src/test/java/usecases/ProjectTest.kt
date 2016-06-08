c6a3a568e511c26aac5c39e6d444e93eb93e35b6
David Edwards, Desmond Pompa Alarcon Rawls, Mark McDonald and Taavo Smith <pair+dedwards+drawls+mmcdonald+tsmith@pivotallabs.com>
2016-06-08 12:48:17 -0400

    Up to uniqueness validation

--- src/test/java/usecases/ProjectTest.kt
+++ src/test/java/usecases/ProjectTest.kt
@@ -3,36 +3,115 @@ package usecases
 import org.assertj.core.api.KotlinAssertions.assertThat
 import org.junit.Assert
 import org.junit.Test
+import java.util.*
 
 class ProjectTest {
 
     @Test
     fun name_is_required() {
         val guiSpy = GuiSpy()
-        create_project(name= "", gui=guiSpy)
+        create_project(name= "", gui=guiSpy, repo = InMemoryProjectRepository())
 
-        assertThat(guiSpy.spyValidationErrors).asList().contains(mapOf("field" to "name", "validation" to "required"))
+        assertThat(guiSpy.validationFailedCalls).asList().contains(mapOf("field" to "name", "validation" to "required"))
+        assertThat(guiSpy.validationFailedCalls.size).isEqualTo(1)
+        assertThat(guiSpy.createSucceededCalls.size).isEqualTo(0)
     }
 
-    private fun create_project(name: String, gui: Gui) {
-        if (name.isEmpty()) {
-            gui.validation_failed(ValidationErrors(listOf(mapOf("field" to "name", "validation" to "required"))))
+    @Test
+    fun save_and_retrieve() {
+        val guiSpy = GuiSpy()
+        val repo = InMemoryProjectRepository()
+        create_project(name = "A project name", gui=guiSpy, repo = repo)
+
+        assertThat(guiSpy.createSucceededCalls.size).isEqualTo(1)
+        assertThat(guiSpy.validationFailedCalls.size).isEqualTo(0)
+
+        assertThat(repo.findByName(guiSpy.createSucceededCalls[0])).isNotNull()
+    }
+
+    @Test
+    fun name_is_unique() {
+        val repo = InMemoryProjectRepository()
+        create_project(name = "A project name", gui=GuiSpy(), repo = repo)
+
+        val guiSpy = GuiSpy()
+        create_project(name = "A project name", gui=guiSpy, repo = repo)
+
+        assertThat(guiSpy.validationFailedCalls).asList().contains(mapOf("field" to "name", "validation" to "unique"))
+        assertThat(guiSpy.validationFailedCalls.size).isEqualTo(1)
+        assertThat(guiSpy.createSucceededCalls.size).isEqualTo(0)
+    }
+
+    private fun create_project(name: String, gui: Gui, repo: ProjectRepository) {
+        if (repo.findByName(name) != null) {
+            gui.validation_failed(ValidationErrors(listOf(mapOf("field" to "name", "validation" to "unique"))))
+        } else if (name.isEmpty()) {
+                gui.validation_failed(ValidationErrors(listOf(mapOf("field" to "name", "validation" to "required"))))
+            } else {
+                val project = repo.create(name)
+                gui.createSucceeded(project.name)
+            }
         }
+
+    }
+
+class InMemoryProjectRepositoryTest {
+
+    @Test
+    fun save_and_retrieve() {
+        val repo = InMemoryProjectRepository()
+
+        repo.create("the name")
+        assertThat(repo.findByName("the name")).isNotNull()
+    }
+}
+
+class InMemoryProjectRepository : ProjectRepository {
+    val projects = HashMap<String, Project>()
+
+    override fun create(s: String) : Project {
+        val project = Project(s)
+        projects[s] = project
+
+        return project
+    }
+
+    override fun findByName(name: String): Project? {
+        return projects[name]
     }
 
 }
 
+interface ProjectRepository {
+
+    fun findByName(name: String) : Project?
+
+    fun create(s: String) : Project
+
+}
+
+data class Project(val name: String) {
+
+}
+
 class GuiSpy() : Gui {
-    override fun validation_failed(errors: ValidationErrors) {
-        spyValidationErrors = errors
+
+    override fun createSucceeded(id: String) {
+        createSucceededCalls.add(id)
     }
 
-    var spyValidationErrors: List<Map<String,String>> = listOf()
+    override fun validation_failed(errors: ValidationErrors) {
+        validationFailedCalls = errors
+    }
+
+    var validationFailedCalls: List<Map<String,String>> = listOf()
+    val createSucceededCalls = ArrayList<String>()
 }
 
 interface Gui {
 
     fun validation_failed(errors: ValidationErrors)
+    fun createSucceeded(id: String)
 
 }
 
