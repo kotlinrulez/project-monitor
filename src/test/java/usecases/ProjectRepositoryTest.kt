0a8ac8266f3a6a1f06a991532029db34be88109a
Mark McDonald <pair+mark.mcdonald.ga@gmail.com>
2016-06-20 23:52:24 -0400

    Replace Test Template with Parameterized Test Runner

--- src/test/java/usecases/ProjectRepositoryTest.kt
+++ src/test/java/usecases/ProjectRepositoryTest.kt
@@ -1,17 +1,26 @@
 package usecases
 
-import org.assertj.core.api.KotlinAssertions
 import org.assertj.core.api.KotlinAssertions.assertThat
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
 
-abstract class ProjectRepositoryTest {
+@RunWith(Parameterized::class)
+class ProjectRepositoryTest {
 
-    abstract fun createProjectRepository() : ProjectRepository
+    companion object {
+        @Parameterized.Parameters
+        @JvmStatic
+        fun repos(): List<ProjectRepository>{
+            return listOf(InMemoryProjectRepository())
+        }
+    }
+
+    @Parameterized.Parameter
+    lateinit var repo: ProjectRepository;
 
     @Test
     fun save_and_retrieve() {
-        val repo = createProjectRepository()
-
         repo.save("twitter for uber")
         repo.save("cats for dogs")
         assertThat(repo.findByName("twitter for uber")).isNotNull()
@@ -20,8 +29,6 @@ abstract class ProjectRepositoryTest {
 
     @Test
     fun save_creates_a_unique_id() {
-        val repo = createProjectRepository()
-
         val twitterForUber = repo.save("twitter for uber")
         val catsForDogs = repo.save("cats for dogs")
         assertThat(twitterForUber.id).isGreaterThan(0)
@@ -31,8 +38,6 @@ abstract class ProjectRepositoryTest {
 
     @Test
     fun save_and_retrieve_all() {
-        val repo = createProjectRepository()
-
         repo.save("twitter for uber")
         repo.save("cats for dogs")
         val projects = repo.findAll()
